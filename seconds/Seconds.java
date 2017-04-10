38a6b1a803436943a162237b90b98204e36e5f51
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-04-10 14:40:35 +0200

    add seconds function

--- seconds/Seconds.java
+++ seconds/Seconds.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Seconds {
   public static void main(String[] args) {
     // Create a function that takes a list as a parameter,
     // and returns a new list with every second element from the orignal list
     // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
+    List<Integer> listOfIntegers = new ArrayList<>();
+    
   }
+  
+  public List<Integer> everySecondElement (List<Integer> originalList) {
+    List <Integer> secondList = new List<>();
+    
+    for (int i = 1; i < originalList.size(); i +=2) {
+      secondList.add(originalList.get(i));
+    }
+    return secondList;
+  }
+  
+  
 }
