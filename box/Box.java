aa4a83d9c356dec5dc860d3e7ee6c56aef86c6da
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-04-10 15:58:19 +0200

    add cubiod constructor

--- box/Box.java
+++ box/Box.java
@@ -3,4 +3,14 @@ public class Cuboid {
   // It should take its three dimensions as constructor parameters (numbers)
   // It should have a method called `getSurface` that returns the cuboid's surface
   // It should have a method called `getVolume` that returns the cuboid's volume
+  int length;
+  int width;
+  int height;
+  
+  Cuboid (int length, int width, int height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+  
 }
